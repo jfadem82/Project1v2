f38c93e4c3f387dfcf1f8bcecd5f128b388d1571
Josh <jfadem82@gmail.com>
2015-12-01 17:22:25 -0800

    object_oriented_version_hopelessly_lost_on_line_17

---
+++ script.js
@@ -0,0 +1,99 @@
+var game = {
+
+	valueLookUpTable: {
+		"A": 1,
+		"K": 10,
+		"Q": 10,
+		"J": 10,
+		"T": 10
+	},
+
+	getCardValue: function(card){
+		if(this.valueLookUpTable[card.face]) {
+			return (this.valueLookUpTable[card.face])
+		} 
+		else return card.face;
+	},
+	// getHandValue: function(hand){
+	// 	var sum = 0;
+
+	// 	if (hand.indexOf("A") >= 0) {
+	// 		for(var i =0; i < hand.length; i++) {
+	// 			sum += this.getCardValue(hand[i])
+	// 		}
+	// 		if(sum <= 11){
+	// 			sum += 10
+	// 		} 
+	// 	} else {
+	// 		for(var i =0; i < hand.length; i++) {
+	// 			sum += this.getCardValue(hand[i])
+	// 		}
+	// 	}
+	// 	return sum;
+	// },
+
+	getAceValue: function(){
+
+	},
+
+
+	player: {
+		hand: []
+		// value: 0,
+		// calcValue: function() {
+
+		// }
+
+	},
+
+	computer: {
+		hand: []
+		// value: 0,
+		// calcValue: function() {
+
+
+		// }
+	},
+
+	deck: {
+		cards: [],
+
+		deal: function(person) {
+			this.hit(person);
+			this.hit(person);
+		},
+
+		hit: function(person){
+			var card = Math.floor((Math.random() * this.cards.length));
+			person.hand.push(this.cards.splice(card, 1)[0]);
+		}
+		
+	}
+}
+
+
+
+
+function Card(suit, face) {
+	this.suit = suit,
+	this.face = face
+}
+
+function makeCards(){
+	var suits = ["d", "c", "h", "s"]
+	var faces = ["A", "K", "Q", "J", "T", 9, 8, 7, 6, 5, 4, 3, 2, ]
+	for(var i = 0; i < suits.length; i++){
+		for(var j = 0; j < faces.length; j++){
+			card = new Card(suits[i], faces[j])
+			game.deck.cards.push(card)
+		}
+	}
+}
+
+function init(){
+	makeCards()
+	game.deck.deal(game.player)
+	game.deck.deal(game.computer)
+}
+
+init()
